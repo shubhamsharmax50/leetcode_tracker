2dc13ecd21389431204443652b41268802978730
shubham sharma <134059974+shubhamsharmax50@users.noreply.github.com>
2026-01-16 20:59:44 +0530

    Create 2645-PassThePillow

---
+++ 2645-PassThePillow/2645-PassThePillow.cpp
@@ -0,0 +1,18 @@
+// Last updated: 16/1/2026, 8:59:45 pm
+class Solution {
+public:
+    int passThePillow(int n, int time) {
+        int idx  = 1;
+        int dir  = 1;
+
+        while(time > 0) {
+            if(idx + dir >=1 && idx + dir <= n) {
+                idx += dir;
+                time--;
+            } else {
+                dir *= -1;
+            }
+        }
+        return idx;
+    }
+};

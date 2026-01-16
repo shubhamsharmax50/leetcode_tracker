789b3e83563dda2b5cf63062e99002239603c4b8
shubham sharma <134059974+shubhamsharmax50@users.noreply.github.com>
2026-01-16 20:58:57 +0530

    Create 3195-SeparateBlackAndWhiteBalls

---
+++ 3195-SeparateBlackAndWhiteBalls/3195-SeparateBlackAndWhiteBalls.cpp
@@ -0,0 +1,20 @@
+// Last updated: 16/1/2026, 8:58:58 pm
+class Solution {
+public:
+    long long minimumSteps(string s) {
+           int n = s.length();
+
+        long long swap  = 0;
+        int black = 0;
+
+        for(int i = 0; i < n; i++) {
+            if(s[i] == '0') { //move white to left
+                swap += black;
+            } else {
+                black++;
+            }
+        }
+
+        return swap;
+    }
+};

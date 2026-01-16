07e145c5dfb95b510117b549031197796aee86df
shubham sharma <134059974+shubhamsharmax50@users.noreply.github.com>
2026-01-16 20:58:27 +0530

    Create 3469-MaximumHeightOfATriangle

---
+++ 3469-MaximumHeightOfATriangle/3469-MaximumHeightOfATriangle.cpp
@@ -0,0 +1,23 @@
+// Last updated: 16/1/2026, 8:58:27 pm
+class Solution {
+public:
+    int helper (int r,int b){
+     int c =1;
+        int rows=0;
+        while(r>=0 && b>=0){
+            if(c%2==1)
+                r -= c;
+            else
+                b -= c;
+            if(r<0 || b<0 )return rows ;
+            rows++;
+                c++;
+        }
+        return rows ;
+    }
+    int maxHeightOfTriangle(int red, int blue) {
+        int ans =0;
+        ans=max(helper(red,blue),helper(blue,red));
+        return ans ;
+    }
+};

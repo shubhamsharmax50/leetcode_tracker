bcc1ae4e229fcfc23f179a11f66b8cd0a9293b20
shubham sharma <134059974+shubhamsharmax50@users.noreply.github.com>
2026-01-16 20:59:11 +0530

    Create 2856-CountCompleteSubarraysInAnArray

---
+++ 2856-CountCompleteSubarraysInAnArray/2856-CountCompleteSubarraysInAnArray.cpp
@@ -0,0 +1,33 @@
+// Last updated: 16/1/2026, 8:59:12 pm
+class Solution {
+public:
+    int countCompleteSubarrays(vector<int>& nums) {
+        int n = nums.size();
+                unordered_set<int> st(begin(nums), end(nums));
+
+                        int c = st.size();
+
+                                int i = 0;
+                                        int j = 0;
+                                                int res = 0;
+
+                                                        unordered_map<int, int> mp;
+                                                                while(j < n) {
+                                                                            mp[nums[j]]++;
+
+                                                                                        while(mp.size() == c) {
+                                                                                                        res += (n-j);
+
+                                                                                                                        mp[nums[i]]--;
+                                                                                                                                        if(mp[nums[i]] == 0) {
+                                                                                                                                                            mp.erase(nums[i]);
+                                                                                                                                                                            }
+                                                                                                                                                                                            i++;
+                                                                                                                                                                                                        }
+
+                                                                                                                                                                                                                    j++;
+                                                                                                                                                                                                                            }
+
+                                                                                                                                                                                                                                    return res;
+    }
+};

a4dc879c7930a7b268d317146331ca9432ea869b
Shubham sharma <134059974+shubhamsharmax50@users.noreply.github.com>
2026-02-26 21:11:36 +0530

    Create 2707-MergeTwo2dArraysBySummingValues

---
+++ 2707-MergeTwo2dArraysBySummingValues/2707-MergeTwo2dArraysBySummingValues.cpp
@@ -0,0 +1,34 @@
+// Last updated: 26/2/2026, 9:11:39 pm
+class Solution {
+public:
+    vector<vector<int>> mergeArrays(vector<vector<int>>& nums1, vector<vector<int>>& nums2) {
+        int m = nums1.size();
+                int n = nums2.size();
+
+                        map<int, int> mp;
+                                vector<vector<int>> result;
+
+                                        for(int i = 0; i < m; i++) {
+                                                    int id  = nums1[i][0];
+                                                                int val = nums1[i][1];
+
+                                                                            mp[id] += val;
+                                                                                    }
+
+                                                                                            for(int i = 0; i < n; i++) {
+                                                                                                        int id  = nums2[i][0];
+                                                                                                                    int val = nums2[i][1];
+
+                                                                                                                                mp[id] += val;
+                                                                                                                                        }
+
+                                                                                                                                                for(auto &it : mp) {
+                                                                                                                                                            int key = it.first;
+                                                                                                                                                                        int val = it.second;
+
+                                                                                                                                                                                    result.push_back({key, val});
+                                                                                                                                                                                            }
+
+                                                                                                                                                                                                    return result;
+    }
+};

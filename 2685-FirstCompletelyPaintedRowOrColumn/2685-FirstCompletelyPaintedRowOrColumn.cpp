961f35e04564c90b365ec6ff898d1587f159ae38
Shubham sharma <134059974+shubhamsharmax50@users.noreply.github.com>
2026-02-26 21:11:44 +0530

    Create 2685-FirstCompletelyPaintedRowOrColumn

---
+++ 2685-FirstCompletelyPaintedRowOrColumn/2685-FirstCompletelyPaintedRowOrColumn.cpp
@@ -0,0 +1,34 @@
+// Last updated: 26/2/2026, 9:11:47 pm
+class Solution {
+public:
+
+    int firstCompleteIndex(vector<int>& arr, vector<vector<int>>& mat) {
+        int m = mat.size();
+        int n = mat[0].size();
+
+        unordered_map<int, pair<int, int>> mp; //stores value to cell-coordinate [i][j]
+
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                int val = mat[i][j];
+                mp[val] = {i, j};
+            }
+        }
+
+        vector<int> rowCountPaint(m, 0); //[i] = how many painted in row i
+        vector<int> colCountPaint(n, 0); //[i] = how many painted in col i
+
+        for(int i = 0; i < arr.size(); i++) {
+            int val = arr[i];
+            auto [row, col] = mp[val];
+
+            rowCountPaint[row]++;
+            colCountPaint[col]++;
+
+            if(rowCountPaint[row] == n || colCountPaint[col] == m) {
+                return i;
+            }
+        }
+        return -1;
+    }
+};

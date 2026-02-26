ab92a14aedf1e025aca04388a00103fc7d7cb1bb
Shubham sharma <134059974+shubhamsharmax50@users.noreply.github.com>
2026-02-26 21:12:19 +0530

    Create 2488-DivideIntervalsIntoMinimumNumberOfGroups

---
+++ 2488-DivideIntervalsIntoMinimumNumberOfGroups/2488-DivideIntervalsIntoMinimumNumberOfGroups.cpp
@@ -0,0 +1,22 @@
+// Last updated: 26/2/2026, 9:12:21 pm
+class Solution {
+public:
+    int minGroups(vector<vector<int>>& intervals) {
+        sort(begin(intervals), end(intervals));
+
+        priority_queue<int, vector<int>, greater<int>> pq; //stores end point of groups
+
+        for(vector<int>& interval : intervals) {
+            int start = interval[0];
+            int end   = interval[1];
+
+            if(!pq.empty() && pq.top() < start) {
+                pq.pop();
+            }
+            pq.push(end);
+        }
+
+        return pq.size();
+     
+    }
+};

1fba489be58157aea0374507dd1077ba9cd4b9f6
Shubham sharma <134059974+shubhamsharmax50@users.noreply.github.com>
2026-02-26 21:12:30 +0530

    Create 2463-MinimumRecolorsToGetKConsecutiveBlackBlocks

---
+++ 2463-MinimumRecolorsToGetKConsecutiveBlackBlocks/2463-MinimumRecolorsToGetKConsecutiveBlackBlocks.cpp
@@ -0,0 +1,30 @@
+// Last updated: 26/2/2026, 9:12:33 pm
+class Solution {
+public:
+    int minimumRecolors(string blocks, int k) {
+      int n = blocks.size();
+              int i = 0;
+                      int j = 0;
+
+                              int ops = k;
+                                      int W = 0;
+
+                                              while(j < n) {
+                                                          if(blocks[j] == 'W') {
+                                                                          W++;
+                                                                                      }
+
+                                                                                                  if(j - i + 1 == k) {
+                                                                                                                  ops = min(ops, W);
+
+                                                                                                                                  if(blocks[i] == 'W') {
+                                                                                                                                                      W--;
+                                                                                                                                                                      }
+                                                                                                                                                                                      i++;
+                                                                                                                                                                                                  }
+                                                                                                                                                                                                              j++;
+                                                                                                                                                                                                                      }
+
+                                                                                                                                                                                                                              return ops;  
+    }
+};

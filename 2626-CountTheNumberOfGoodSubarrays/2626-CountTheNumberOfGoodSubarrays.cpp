e69bc68a41fa85c6509a19a12255c278f5f02624
Shubham sharma <134059974+shubhamsharmax50@users.noreply.github.com>
2026-02-26 21:11:50 +0530

    Create 2626-CountTheNumberOfGoodSubarrays

---
+++ 2626-CountTheNumberOfGoodSubarrays/2626-CountTheNumberOfGoodSubarrays.cpp
@@ -0,0 +1,31 @@
+// Last updated: 26/2/2026, 9:11:52 pm
+class Solution {
+public:
+    long long countGood(vector<int>& nums, int k) {
+        int n = nums.size();
+                int i = 0;
+                        int j = 0;
+
+                                long long result = 0;
+
+                                        unordered_map<int, int> mp;
+
+                                                long long pairs = 0;
+
+                                                        while(j < n) {
+                                                                    pairs += mp[nums[j]]; //isse pehle kitni baar nums[j] dekha hai humne
+                                                                                mp[nums[j]]++;
+
+                                                                                            while(pairs >= k) {
+                                                                                                            result += (n-j);
+                                                                                                                            mp[nums[i]]--;
+                                                                                                                                            pairs -= mp[nums[i]];
+                                                                                                                                                            i++;
+                                                                                                                                                                        }
+
+                                                                                                                                                                                    j++;
+                                                                                                                                                                                            }
+
+                                                                                                                                                                                                    return result;
+    }
+};

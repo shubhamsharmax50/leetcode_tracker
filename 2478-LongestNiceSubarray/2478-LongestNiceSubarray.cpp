e0f8a629347e0d239205cdbfb65cd96271e487c3
Shubham sharma <134059974+shubhamsharmax50@users.noreply.github.com>
2026-02-26 21:12:22 +0530

    Create 2478-LongestNiceSubarray

---
+++ 2478-LongestNiceSubarray/2478-LongestNiceSubarray.cpp
@@ -0,0 +1,27 @@
+// Last updated: 26/2/2026, 9:12:25 pm
+class Solution {
+public:
+    int longestNiceSubarray(vector<int>& nums) {
+        int n = nums.size();
+
+                int i = 0;
+                        int j = 0;
+
+                                int result = 1;
+                                        int mask = 0;
+
+                                                while(j < n) {
+
+                                                            while((mask & nums[j]) != 0) { //keep shrinking
+                                                                            mask = (mask ^ nums[i]);
+                                                                                            i++;
+                                                                                                        }
+
+                                                                                                                    result = max(result, j-i+1);
+                                                                                                                                mask = (mask | nums[j]);
+                                                                                                                                            j++;
+                                                                                                                                                    }
+
+                                                                                                                                                            return result;
+    }
+};

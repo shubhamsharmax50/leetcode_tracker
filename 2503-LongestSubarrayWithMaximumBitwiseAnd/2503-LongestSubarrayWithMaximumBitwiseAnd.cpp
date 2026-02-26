cae0076eb2ba54ce1cc21ea22ba3ec0b4ade408a
Shubham sharma <134059974+shubhamsharmax50@users.noreply.github.com>
2026-02-26 21:12:14 +0530

    Create 2503-LongestSubarrayWithMaximumBitwiseAnd

---
+++ 2503-LongestSubarrayWithMaximumBitwiseAnd/2503-LongestSubarrayWithMaximumBitwiseAnd.cpp
@@ -0,0 +1,27 @@
+// Last updated: 26/2/2026, 9:12:17 pm
+class Solution {
+public:
+    int longestSubarray(vector<int>& nums) {
+        int maxVal=0;
+
+        int result=0;
+
+        int streak=0;
+        for(int num:nums){
+            if(num>maxVal){
+                maxVal=num;
+                result=0;
+                streak=0;
+
+            }
+        
+        if(maxVal==num){
+            streak++;
+        }else{
+            streak=0;
+        }
+        result=max(result,streak);
+    }
+    return result;
+}
+};
